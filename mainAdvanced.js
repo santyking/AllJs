48066deca48e35df6b3b2a905bf631a560fce6b8
Santosh.Mhatre <santosh.mhatre93@gmail.com>
2024-01-03 21:48:50 +0530

    Added Output to Main advanced for reference

--- mainAdvanced.js
+++ mainAdvanced.js
@@ -1,4 +1,4 @@
-// nested or lexical scoping
+// ***** nested or lexical scoping *****
 // let a = 10;
 // function outer() {
 //     let b = 5;
@@ -9,107 +9,82 @@
 //     inner(); 
 // }
 // outer();
-// function outer() {
-//     let a = 0;
-//     function inner() {
-//         a++;
-//         console.log(a);
-//     }
-//     inner();
-// }
-// outer();
-// outer();
-// javascript closure
-// function outer() {
-//     let a = 0;
-//     function inner() {
-//         a++;
-//         console.log(a);
-//     }
-//     return inner;
-// }
-// let res = outer();
-// res();
-// res();
-// currying
-// sum = (a,b,c) =>{
-//     return a+b+c;
-// }
-// // console.log(sum(2,2,2));
-// function curry(fn) {
-//     return function(a){
-//         return function(b){
-//             return function(c){
-//                 return fn(a,b,c)
-//             }
-//         }
-//     }
-    
-// }
-// const curriedsum = curry(sum);
-// console.log(curriedsum(2)(2)(2));
-// closure practice
-// function outer() {
-//     let counter = 0;
-//     function inner() {
-//         counter++;
-//         console.log(counter);
-//     }
-//     return inner;
-// }
-// let sm = outer();
-// sm();
-// sm();
-// sm();
-// sm();
-// sm();
-// currying
-// function sum(a,b,c) {
-//     return a+b+c;
-// }
-// // console.log(sum(2,2,2));
-// function curry(fn) {
-//     return function(a){
-//         return function(b){
-//             return function(c){
-//                 return fn(a,b,c);     
-//             }
-//         }
-//     }
-// }
-// const curriedsum = curry(sum);
-// console.log(curriedsum(2)(2)(2));
+// op : 10 5 7
 
-// let add2 = curriedsum(2);
-// let add3 = add2(2);
-// let add4 = add3(2);
-// console.log(add4);
-function sum(a,b,c) {
+// ***** Closure *****
+/*function outer() {
+    let a = 0;
+    function inner() {
+        a++;
+        console.log(a);
+    }
+    inner();
+}
+outer();
+outer();
+Op:
+1
+1
+*/
+
+
+// ***** javascript closure *****
+/*
+function outer() {
+    let a = 0;
+    function inner() {
+        a++;
+        console.log(a);
+    }
+    return inner;
+}
+let res = outer();
+res();
+res();
+Op:
+1
+2
+*/
+
+/*
+// ***** currying *****
+sum = (a,b,c) =>{
     return a+b+c;
 }
-function curry2(fn){
-    return function fn1(a) {
-        return function fn2(b) {
-            return function fn3(c) {
-                return fn(a,b,c);
+// console.log(sum(2,2,2)); // Op : 6
+// This will give output as 6, but we want to have curry function for better error handling
+
+function curry(fn) {
+    return function(a){
+        return function(b){
+            return function(c){
+                return fn(a,b,c)
             }
         }
-        
     }
+    
 }
-let curriedsum = curry2(sum);
-console.log(curriedsum(2)(2)(2));
-// second way to curry a fucntion
+const curriedsum = curry(sum);
+// first way
+// console.log(curriedsum(2)(2)(2));
+
+// // second way to curry a fucntion
 let add2 = curriedsum(2);
 let add3 = add2(2);
 let add4 = add3(2);
 console.log(add4);
-// This keyword
+// Op: 6
+*/
+
+// ***** This keyword *****
 // function greeter(name) {
 //     console.log(`My name is ${name}`);
 // }
 // greeter("Santosh");
 // greeter("Ankita");
+// Op:
+// My name is Santosh
+// My name is Ankita
 
 // Implicit Binding
 // const person = {
@@ -120,6 +95,7 @@ console.log(add4);
 //     }
 // }
 // person.sayName();
+// My name is Santosh
 // when a function is called with this keyword, the object name to the left is what this keyword is refering to in implicit binding
 
 // In Explicit Binding
@@ -131,6 +107,7 @@ console.log(add4);
 //     console.log(`Hello!My name is ${this.name}`);
 // }
 // sayMyName.call(person);
+//Op: Hello!My name is Santosh
 
 // new binding
 // when we invoke a function with new keyword, this keyword will reference an empty obj
@@ -142,9 +119,18 @@ console.log(add4);
 // const p1 = new person("Santosh");
 // const p2 = new person("Ankita");
 // console.log(p1.name,p2.name);
+// Santosh Ankita
+
 // default binding
+// globalThis.name = "Ironman";
+// function sayMyName(name) {
+//     console.log(`Hello!My name is ${this.name}`);
+// }
 // sayMyName();
+// Op: Hello!My name is Ironman
 // if nothing matches, the binding is default binding in which the system relies on the globalThis
+
+
 // anonymous function
 // let multiplication = function(a,b){
 //     let mul = a*b;
@@ -152,7 +138,9 @@ console.log(add4);
 //     console.log(`The Multiplication of ${a} and ${b} is : ${mul}`);
 // };
 // multiplication(3,4);
-// prototype
+
+// ***** prototype *****
+/*
 function displayDetails(fname,lname) {
     this.firstName = fname;
     this.lastName = lname;
@@ -160,17 +148,18 @@ function displayDetails(fname,lname) {
 const d1 = new displayDetails("Santosh","Mhatre");
 const d2 = new displayDetails("Ankita","Mhatre");
 // Since js is a dynamic language,we can extend the properties here
-// d1.getFullname = function () {
-//     return this.firstName + ' ' + this.lastName; 
-// }
-// console.log(d1.getFullname());
-// op is Santosh Mhatre
+d1.getFullname = function () {
+    return this.firstName + ' ' + this.lastName; 
+}
+console.log(d1.getFullname());
+// Op: Santosh Mhatre
 // but if we try 
 // console.log(d2.getFullname());
 // we get error
 // so we use prototype such that we can use for any n number of objects
-displayDetails.prototype.getFullname = function() {
-    return this.firstName+' '+this.lastName;
-}
-console.log(d1.getFullname());
-// op is Santosh Mhatre
+// displayDetails.prototype.getFullname = function() {
+//     return this.firstName+' '+this.lastName;
+// }
+// console.log(d1.getFullname());
+// Op: Santosh Mhatre
+*/
---
+++ prac3Jan24.js
@@ -0,0 +1,12 @@
+// Lexical Scoping
+// let a = 5;
+// function first() {
+//     let b =10;
+//     function second() {
+//         let c = 20;
+//         console.log(a,b,c);
+//     }
+//     second();
+// }
+// first();
+// closure
