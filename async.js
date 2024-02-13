d6eb2819b3cfb9972c234738509d75563b93ca71
Santosh.Mhatre <santosh.mhatre93@gmail.com>
2024-02-13 23:29:22 +0530

    callback functions

--- async.js
+++ async.js
@@ -24,8 +24,29 @@ function greet(name) {
 setInterval(greet,2000,"Santosh");
 */
 // clear setInterval
+/*
 function greet(name) {
     console.log(`Hello ${name}`);
 }
 const intervalId  = setInterval(greet,2000,"Santosh");
 clearInterval(intervalId);
+*/
+// synchronous callback
+/* 
+function greet(name) {
+    console.log(`Hello ${name}`);
+}
+function greetPerson(greetfn) {
+    const name = "Santosh";
+    greetfn(name);
+}
+greetPerson(greet);
+*/ 
+// Asynchronous Callback
+/*
+function greet(name) {
+    console.log(`Hello ${name}`);
+}
+ // here higher order function
+setTimeout(greet,2000,"Santosh");
+*/
