1093f13257e3b184f21fe7f54b9d3e18faf338ca
Santosh.Mhatre <santosh.mhatre93@gmail.com>
2024-04-02 22:57:16 +0530

    promises practice

--- asyncPractice.js
+++ asyncPractice.js
@@ -49,6 +49,7 @@ let myPromise = new Promise((myResolve, myReject) => {
 })
 */
 //promises
+/*
 let calculator = (num1,num2) => {
     return new Promise((resolve, reject) => {
         let sum = num1+num2;
@@ -69,3 +70,21 @@ let errorOccured = (error) => {
 calculator(5,10)
 .then(DisplayResult)
 .catch(errorOccured);
+*/
+let calculator = new Promise((resolve, reject) => {
+    let num1 = 10;
+    let num2 = 5;
+    let sum = num1+num2;
+    if (sum>=0) {
+        resolve(sum);
+    }
+    else{
+        reject();
+    }
+});
+calculator.then((sum)=>{
+    console.log(sum);
+})
+.catch(()=>{
+    console.log("incorrect output");
+})
